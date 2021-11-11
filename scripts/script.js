2738ad91dd02ed5ce7eda0ab51b22410199614fc
maximvh13 <90190991+maximvh13@users.noreply.github.com>
2021-11-11 14:42:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,32 @@
 // JavaScript Document
 
 
-var deButton = document.querySelector("header nav button");
+var deMenuButton = document.querySelector("header nav button");
 var deNav = document.querySelector("header nav");
 
-deButton.addEventListener("click", toggleMenu);
+deMenuButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(event) {
   deNav.classList.toggle("toonMenu");
+}
+
+var deChatButton = document.querySelector("main aside button");
+
+deChatButton.addEventListener("click", openChat);
+
+function openChat(){
+  console.log("openchat");
+	let hetFormulier = document.querySelector("form.chat-form");
+  hetFormulier.classList.add("toonForm");
+  deButton.classList.add("toonForm");
+}
+
+var deSluitButton = document.querySelector("main aside form button");
+
+deSluitButton.addEventListener("click", sluitChat);
+
+function sluitChat(){
+	let hetFormulier = document.querySelector("form.chat-form");
+  hetFormulier.classList.remove("toonForm");
+  deButton.classList.remove("toonForm");
 }
